ba4a4a80474b9dcfc66f341a1ba8749dfac67912
Travis Heller <trav.heller@gmail.com>
2014-03-06 00:01:47 -0600

    stuff

--- js/getIGfeed.js
+++ js/getIGfeed.js
@@ -1,3 +1,6 @@
+//https://api.instagram.com/v1/tags/fuckyeah/media/recent?client_id=4a21d77050354080b0269479931553ad
+
+/* ---------- OBJECTS ----------*/
 
 $scope.feed = [
 	{
@@ -8,16 +11,47 @@ $scope.feed = [
 	}
 ]
 
+$scope.instagram = [
+	{
+
+	}
+]
+
+
+var igPost = {
+	photoUrl: "",
+	title: "",
+	author: "",
+	source: "instagram",
+}
+
 var igTagURL = 'https://api.instagram.com/v1/tags/',
 	recentTags = '/media/recent?',
-	clientID = 'client_id=4a21d77050354080b0269479931553ad'
+	tagName = 'fuckyeah',
+	clientID = 'client_id=4a21d77050354080b0269479931553ad';
 
 
-https://api.instagram.com/v1/tags/fuckyeah/media/recent?client_id=4a21d77050354080b0269479931553ad
 
 
-$http.get('/someUrl').success(successCallback);
-jsonList.foreach ( function (post){
+var igPostFeed = $http.get(igTagURL+tagName+recentTags+clientID).success(successCallback);
+console.log(igTags);
+
+
+
+
+igPosts.foreach ( function (post){
 	var newIG = newPost(jsonlist.photourl,title,author,source);
 	$scope.feed.unshift(newIG);
 });
+
+
+
+function newPost (post){
+	var newPost = Object.create(igPost),
+		url = post.images.standard_resolution.url;
+		title = post.caption.text,
+		author = post.caption.from.username;
+	newPost.photoUrl = url;
+	newPost.title = title;
+	newPost.author = author;
+}
